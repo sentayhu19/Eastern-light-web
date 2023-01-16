7827cc3e5af9a93725a8c2ef3964901415f9b341
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-17 00:48:41 +0300

    Hero scroll added

--- src/Components/Scroll.jsx
+++ src/Components/Scroll.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
+import slide1 from '../assets/spinetta.jpg';
+import slide2 from '../assets/karolina.jpg';
+import slide3 from '../assets/grabowska.jpg';
+import slide5 from '../assets/slide5.jpg';
 
 const Scroll = () => {
     const slideImages = [
         {
-          url: './assets/spinetta.jpg',
+          url: slide1,
           caption: 'Easter Light Phrma'
         },
         {
-          url: 'images/slide_3.jpg',
-          caption: 'Slide 2'
+          url: '../assets/karolina.jpg',
+          caption: slide5,
         },
         {
-          url: 'images/slide_4.jpg',
-          caption: 'Slide 3'
+          url: '../assets/karolina.jpg',
+          caption: slide3,
         },
       ];
   return (
-    <div className="h-screen">
+    <div className="slide-container">
     <Slide>
      {slideImages.map((slideImage, index)=> (
         <div className="h-screen  each-slide" key={index}>
           <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-            <span className=''>{slideImage.caption}</span>
+            {/* <img src={slideImage.url} alt={slideImage.caption} className='h-screen w-full object-cover' /> */}
+            <span className='text-red-500 bg-slate-50'>{slideImage.caption}</span>
           </div>
         </div>
       ))} 
