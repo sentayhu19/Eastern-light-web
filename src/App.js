75f1cb263c28bebf6aa483639fdfea2dc3bf96db
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-17 22:24:19 +0300

    Service section updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import './App.css';
-
+import Services from './Components/Services';
 import Hero from './Components/Hero';
 function App() {
   return (
     <div className="App">
      <Hero/>
+      <Services/>
     </div>
   );
 }
---
+++ src/Components/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section>
+        <p className='text-2xl font-bold'>Our Services</p>
+        <p>Distribute phrma and biomedical equipments </p>
+    
+    </section>
+
+  )
+}
+export default Services;
