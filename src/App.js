05660b7e4f0062f8d8082aab72cbb1bdfee39dd6
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-17 21:22:08 +0300

    Hero section bg updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import './App.css';
+
 import Hero from './Components/Hero';
 function App() {
   return (
--- src/Components/Scroll.jsx
+++ src/Components/Scroll.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
-import slide1 from '../assets/spinetta.jpg';
-import slide2 from '../assets/karolina.jpg';
-import slide3 from '../assets/grabowska.jpg';
+import slide1 from '../assets/slide1.jpg';
+import slide2 from '../assets/slide2.jpg';
+import slide3 from '../assets/slide3.jpg';
+import slide4 from '../assets/slide4.jpg';
 import slide5 from '../assets/slide5.jpg';
+
 
 const Scroll = () => {
     const slideImages = [
         {
           url: slide1,
-          caption: 'Easter Light Phrma'
+          caption: 'Easter Light Phrma',
+          description: 'Easter Light Pharma is a leading supplier of high-quality pharmaceuticals and biomedical equipment.',
         },
         {
-          url: '../assets/karolina.jpg',
-          caption: slide5,
+          url: slide4,
+          caption: 'Advancing medicine, improving lives',
+          description: 'We are dedicated to advancing medicine and improving lives through our wide range of pharmaceuticals and biomedical equipment. We strive to provide the latest and most effective solutions to healthcare professionals.',
         },
         {
-          url: '../assets/karolina.jpg',
-          caption: slide3,
+          url: slide5,
+          caption: 'Leading the way in biomedical innovation',
+          description: 'Easter Light Pharma is a leader in biomedical innovation, supplying cutting-edge pharmaceuticals and medical equipment to the healthcare industry. They are committed to driving progress and improving patient outcomes through their products.',
         },
       ];
   return (
@@ -26,9 +31,15 @@ const Scroll = () => {
     <Slide>
      {slideImages.map((slideImage, index)=> (
         <div className="h-screen  each-slide" key={index}>
-          <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-            {/* <img src={slideImage.url} alt={slideImage.caption} className='h-screen w-full object-cover' /> */}
-            <span className='text-red-500 bg-slate-50'>{slideImage.caption}</span>
+          <div className='h-screen w-full bg-cover bg-center  backdrop-blur-lg' style={{'backgroundImage': `url(${slideImage.url})`}}>
+            <div className='absolute h-screen w-full opacity-90 backdrop-blur-sm'>
+          <div className='absolute mt-36  mx-4 h-auto w-80  bg-black opacity-40 backdrop-blur-sm  '>
+            <div className='flex flex-col gap-6'>
+            <span className=' text-white text-center text-3xl'>{slideImage.caption}</span>
+            <p className='text-center text-sm text-white'>{slideImage.description}</p>
+            </div>
+           </div>
+           </div>
           </div>
         </div>
       ))} 
