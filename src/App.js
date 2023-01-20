03e0552cf886a29d11dfb644579fe843d2bec0a0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-20 21:58:50 +0300

    Loading screen added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import { useEffect } from 'react';
+import {useState, useEffect } from 'react';
 import './App.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import { HashLoader } from 'react-spinners';
 import Services from './Components/Services';
 import Hero from './Components/Hero';
 import Who from './Components/Who';
 import About from './Components/About';
 function App() {
+  const [loading, setloading] =  useState(false);
   useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 4000);
     Aos.init({ duration: 1000, delay: 150 });
   }, []);
   return (
     <div className="App">
-     <Hero/>
+      {loading ? 
+      <div className='flex items-center h-screen justify-center'>      
+      <HashLoader
+      color="#76A900"
+      size={70}
+    /></div>
+      :
+      (
+      <>  
+      <Hero/>
       <Services/>
       <Who/>
       <About/>
+      </>
+      )}
+    
     </div>
   );
 }
