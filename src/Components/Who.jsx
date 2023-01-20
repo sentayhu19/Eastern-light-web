a17f9aca31d7d9292d7f83e4ea25e15ed770d266
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-20 15:21:23 +0300

    Flip left on who section

--- src/Components/Who.jsx
+++ src/Components/Who.jsx
@@ -6,14 +6,14 @@ const Who = () => {
             <p className='text-2xl font-bold pt-7 'data-aos="fade-up">WHO WE ARE</p>
             <p className="sm:p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolore culpa inventore qui quas numquam, 
                 distinctio veritatis, enim debitis accusantium beatae nulla cumque dolores placeat, deleniti consectetur adipisci? Quaerat, pariatur?</p>
-                <div className="flex sm:flex-col sm:m-auto gap-8">
-                        <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]" data-aos="ease-in-cubic">
+                <div className="flex sm:flex-col sm:m-auto gap-8"  data-aos="flip-left">
+                        <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]">
                             <p>4 +</p> &nbsp;&nbsp;&nbsp;&nbsp; Year
                             </div>
-                            <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]">
+                            <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]" >
                             <p>1000+</p> 
                             </div>
-                            <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]" >
+                            <div className=" flex flex-col rounded-full bg-[#006394] w-24 h-24 items-center justify-center text-white font-bold text-[1.6rem]"  >
                             <p>In 5 Cities</p> 
                             </div>
                             
