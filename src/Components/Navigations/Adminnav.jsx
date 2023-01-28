ba296ec489177a36162af3db67bc95148088bac0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-28 17:19:43 +0300

    Product page list updated

--- src/Components/Navigations/Adminnav.jsx
+++ src/Components/Navigations/Adminnav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 const Adminnav = () => {
   return (
     <div className='bg-[#006394] mt-36 py-4 my-6 w-full'>
-      <div className='flex items-center gap-4 md:w-[400px] m-auto sm:w-[200px] sm:flex-col md:flex-row'>
+      <div className='flex items-center gap-4 md:w-[450px] m-auto sm:w-[200px] sm:flex-col md:flex-row'>
       <NavLink  className='hover:bg-[#76A900] rounded-lg' to="/dashboard">
       <button className='text-white font-bold text-lg'>Dashboard</button>
         </NavLink>
