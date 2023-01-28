e817d07b1c006419114f1efa6502100af152d2d8
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-28 17:42:48 +0300

    Products style updated

--- src/Components/Products/Productshow.jsx
+++ src/Components/Products/Productshow.jsx
@@ -17,17 +17,17 @@ const dispatch = useDispatch();
 const { products } = useSelector((state) => state.product);
 
   return (
-    <section className="text-center mt-36">
+    <section className="text-center mt-36 bg-[#F0F1F3]">
     <div className="flex flex-col mb-7 md:mb-20">
     <h2 className='text-2xl font-bold pt-7 'data-aos="fade-up">PRODUCTS</h2>
-    <div className='grid md:grid-cols-4 items-center w-full px-[10%] '>
+    <div className='grid md:grid-cols-4  items-center w-full px-[5%] md:px-[6%] '>
     { products.map((product) => (
-      <div className='flex items-center justify-center flex-col md:flex-row gap-4 m-auto w-[90%] md:w-[80%] shadow-lg mt-10' key={product._id}>
+      <div className='flex items-center justify-center flex-col md:flex-row gap-4 m-auto w-[90%] md:w-[96%] h-[500px] shadow-lg mt-10 p-7 hover:border-2 border-[#76A900] rounded-lg' data-aos="fade-up" key={product._id}>
       <div className='flex flex-col gap-4'>
       <img src={product.image} alt="" className='w-[200px] h-[200px] object-cover'/>
       <div className='border-b'>
-      <h3 className='text-lg font-bold'>{product.name}</h3>
-      <p className='text-lg font-bold'>${product.price}</p>
+      <h3 className='text-lg font-bold w-full'>{product.name}</h3>
+      <p className='text-lg font-bold w-full'>${product.price}</p>
       </div>
       <p>{product.description}</p>
       </div>
