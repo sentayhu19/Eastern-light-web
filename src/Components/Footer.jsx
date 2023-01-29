e3621a72d054f283cc5dafae03d68be1425f6cd0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-01-29 14:03:10 +0300

    Footer style updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,42 +12,44 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className='w-[45%] m-auto pt-10 text-center'>
-        <div className='flex sm:flex-col md:flex-row gap-[10%]   '>
-            <div  className='flex flex-col items-center justify-center gap-4'>
-                <div className='flex flex-col'>
-                <h4 className='font-bold'>Company</h4>
+    <footer className='w-[45%] m-auto pt-10 '>
+        <div className='flex sm:flex-col md:flex-row md:gap-[10%] sm:gap-10   '>
+            <div  className='flex flex-col items-center  gap-4'>
+                <div className='flex flex-col gap-4'>
+                <h4 className='font-bold border-b'>Company</h4>
                 <div className='flex gap-3 items-center' > 
                     <FontAwesomeIcon icon={faLocationDot}/> 
                     <h4>
                     Around paster area</h4> </div>
                     </div>
                 </div>
-            <div >
-                <h4 className='font-bold'>Contact</h4>
+            <div className='flex flex-col gap-4' >
+                <h4 className='font-bold border-b'>Contact</h4>
+                <div>
                 <div className='flex gap-3 items-center'>
                     <FontAwesomeIcon icon={faPhone}/>
                    <h4> +251 00 000 0000</h4></div>
                 <div className='flex gap-3 items-center'>
                     <FontAwesomeIcon icon={faEnvelope}/>
                     <h4> esternlightphrma@gmail.com </h4></div>
-                    <div  className='flex flex-col items-center gap-4'>
-                        <h4 className='font-bold'>Connect with us:</h4>
-                        <div className='flex md:gap-5'>
-                        <a className=' hover:border w-6 h-6 rounded-full' href='https://www.linkedin.com/company/eastern-pharma-plc' target='_blank' rel="noreferrer">
-                        <FontAwesomeIcon icon={faLinkedinIn} className='text-blue-500'/>
+                    <div  className='flex flex-col  gap-4 mt-4'>
+                        <h4 className='font-bold border-b'>Connect with us:</h4>
+                        <div className='flex md:gap-5 sm:gap-10'>
+                        <a className=' hover:border md:w-10 h-10 text-2xl rounded-full text-[#0077b5]  hover:text-white hover:bg-[#0077b5]' href='https://www.linkedin.com/company/eastern-pharma-plc' target='_blank' rel="noreferrer">
+                        <FontAwesomeIcon icon={faLinkedinIn}/>
                         </a>
-                        <a className=' hover:border w-6 h-6 rounded-full' href='https://www.facebook.com/Eastern-Pharma-PLC-100000000000000' target='_blank' rel="noreferrer">
-                        <FontAwesomeIcon icon={faFacebookF} className='text-blue-500'/>
+                        <a className=' hover:border w-10 h-10 rounded-full text-2xl  text-[#3b5999]  hover:text-white hover:bg-[#3b5999]' href='https://www.facebook.com/Eastern-Pharma-PLC-100000000000000' target='_blank' rel="noreferrer">
+                        <FontAwesomeIcon icon={faFacebookF} />
                         </a>
-                        <a className=' hover:border w-6 h-6 rounded-full' href='https://twitter.com/EasternPharma' target='_blank' rel="noreferrer">
-                        <FontAwesomeIcon icon={faTwitter} className='text-blue-500'/>
+                        <a className=' hover:border w-10 h-10 rounded-full text-2xl  text-[#55acee]  hover:text-white hover:bg-[#55acee]' href='https://twitter.com/EasternPharma' target='_blank' rel="noreferrer">
+                        <FontAwesomeIcon icon={faTwitter}/>
                         </a>
                         </div>
                     </div>
+                    </div>
             </div>
             <div>
-                <h2 className='font-bold'>Location on Google map</h2>
+                <h2 className='font-bold border-b'>Location on Google map</h2>
             </div>
         </div>
         <div className='pt-10'>
