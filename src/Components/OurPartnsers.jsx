020dcb2d525126803322a3385d227d5ed76601e2
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-02-05 01:05:24 +0300

    Partners fade style updated

--- src/Components/OurPartnsers.jsx
+++ src/Components/OurPartnsers.jsx
@@ -23,10 +23,10 @@ const Partners= () => {
     },
 ]
         return ( 
-            <div data-aos="fade-up">
+            <div >
                 <div className=" items-center justify-center text-black py-10">
-                    <h1 className="text-center text-3xl font-bold mb-10">OUR PARTNERS</h1>
-                    <div className='grid grid-cols-3  md:w-[60%] sm:w-[90%] m-auto'>
+                    <h1 className="text-center text-3xl font-bold mb-10 "data-aos="fade-up">OUR PARTNERS</h1>
+                    <div className='grid grid-cols-3  md:w-[60%] sm:w-[90%] m-auto' data-aos="fade-up">
                  {   partners.map((partner) => (
                         <div className="flex flex-col items-center justify-center text-black py-10"  >
                             <img className='md:w-40 md:h-40 filter-gray-scale filter-hover-gray-scale ' src={partner.image} alt={partner.name} />
